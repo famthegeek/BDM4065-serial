4f0aad4c8c2c241c69801981ad4eb33055ea5955
Fredrik Mundt <famthegeek95@gmail.com>
2016-03-21 01:19:38 +0100

    initial commit

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ var isOpen = false;
 serialPort.on('open', function(data) {
   console.log('open: ' + data);
 });
-serialPort.open(function (error) {
-  if ( error ) {
-    console.log('failed to open: '+error);
-  } else {
-    console.log('open');
-    var data = new Buffer("A60100000004011802B8","hex");
-    serialPort.write(data, function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });
-  }
+
+var data = new Buffer("A60100000004011802B8","hex");
+serialPort.write(data, function(err, results) {
+  console.log('err ' + err);
+  console.log('results ' + results);
 });
